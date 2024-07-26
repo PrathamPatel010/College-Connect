605ea26f251a197aaf83734b9f979f40e73d4f56
Pratham Patel <ppratham812@gmail.com>
2024-07-26 14:49:45 +0530

    fix:group chats not being fetched issue fixed

--- api/src/repository/chat-repository.ts
+++ api/src/repository/chat-repository.ts
@@ -5,48 +5,48 @@ import {
     CreateOrAccessChatDto,
     UpdateGroupChatDto
 } from "../dto/chat-dto";
-import {BadRequestError} from "../utils/error-handler/AppError";
+import { BadRequestError } from "../utils/error-handler/AppError";
 
-class ChatRepository{
-    async createP2P(data:CreateOrAccessChatDto){
+class ChatRepository {
+    async createP2P(data: CreateOrAccessChatDto) {
         try {
             // check if P2P chat already exists
             const existingChat = await prismaService.chat.findFirst({
-                where:{
-                    AND:[
-                        {isGroupChat:false},
-                        {users:{some:{id:data?.loggedInUserId}}},
-                        {users:{some:{id:data.userId}}}
+                where: {
+                    AND: [
+                        { isGroupChat: false },
+                        { users: { some: { id: data?.loggedInUserId } } },
+                        { users: { some: { id: data.userId } } }
                     ]
                 },
-                include:{
-                    users:{
-                        select:{
-                            id:true,username:true,email:true,pic:true,isVerified:true,
+                include: {
+                    users: {
+                        select: {
+                            id: true, username: true, email: true, pic: true, isVerified: true,
                         }
                     }
                 }
             });
 
-            if (existingChat){
+            if (existingChat) {
                 return existingChat;
             }
 
             const chat = await prismaService.chat.create({
-                data:{
-                    chatName:'sender',
-                    isGroupChat:false,
-                    users:{
-                        connect:[
-                            {id:data?.loggedInUserId},
-                            {id:data.userId}
+                data: {
+                    chatName: 'sender',
+                    isGroupChat: false,
+                    users: {
+                        connect: [
+                            { id: data?.loggedInUserId },
+                            { id: data.userId }
                         ]
                     }
                 },
-                include:{
-                    users:{
-                        select:{
-                            id:true,username:true,email:true,pic:true,isVerified:true,
+                include: {
+                    users: {
+                        select: {
+                            id: true, username: true, email: true, pic: true, isVerified: true,
                         }
                     }
                 }
@@ -60,24 +60,24 @@ class ChatRepository{
 
 
     // for creating group chat
-    async createGroup(data:CreateGroupChatDto){
+    async createGroup(data: CreateGroupChatDto) {
         try {
-            if (data.users.length<3){
+            if (data.users.length < 3) {
                 throw new BadRequestError("Atleast 3 users are required to create group chat");
             }
             const groupChat = await prismaService.chat.create({
-                data:{
-                    chatName:data.chatName,
-                    isGroupChat:true,
-                    groupAdminId:data?.loggedInUser?.id,
-                    users:{
-                        connect: data.users.map(userId=>({id:userId}))
+                data: {
+                    chatName: data.chatName,
+                    isGroupChat: true,
+                    groupAdminId: data?.loggedInUser?.id,
+                    users: {
+                        connect: data.users.map(userId => ({ id: userId }))
                     }
                 },
-                include:{
-                    users:{
-                        select:{
-                            id:true,email:true,username:true,pic:true,isVerified:true
+                include: {
+                    users: {
+                        select: {
+                            id: true, email: true, username: true, pic: true, isVerified: true
                         }
                     }
                 }
@@ -90,20 +90,27 @@ class ChatRepository{
     }
 
     // for fetching all chats of logged in user
-    async fetchChats(userId:number|undefined){
+    async fetchChats(userId: number | undefined) {
         try {
             const chats = await prismaService.chat.findMany({
-                where:{
-                    users:{
-                        some:{
-                            id:userId
+                where: {
+                    OR: [  // Include chats where the user is a member or group chats where the user is the admin
+                        {
+                            users: {
+                                some: {
+                                    id: userId
+                                }
+                            }
+                        },
+                        {
+                            groupAdminId: userId  // Include group chats where the user is the admin
                         }
-                    }
+                    ]
                 },
-                include:{
-                    users:{
+                include: {
+                    users: {
                         select: {
-                            id: true, username: true, email: true, pic: true, isVerified:true
+                            id: true, username: true, email: true, pic: true, isVerified: true
                         }
                     }
                 }
@@ -115,32 +122,32 @@ class ChatRepository{
         }
     }
 
-    async updateChat(data:UpdateGroupChatDto){
+    async updateChat(data: UpdateGroupChatDto) {
         try {
             console.log(data);
             const chat = await prismaService.chat.findFirst({
-                where:{
-                    AND:[
-                        {id:data.chatId},
-                        {groupAdminId:data.loggedInUserId}
+                where: {
+                    AND: [
+                        { id: data.chatId },
+                        { groupAdminId: data.loggedInUserId }
                     ]
                 }
             })
-            if (!chat){
+            if (!chat) {
                 throw new Error("Chat not found OR you are not the group admin");
             }
 
             const updatedGroupChat = await prismaService.chat.update({
-                where:{
-                    id:data.chatId
+                where: {
+                    id: data.chatId
                 },
-                data:{
-                    chatName:data.newChatName
+                data: {
+                    chatName: data.newChatName
                 },
-                include:{
-                    users:{
-                        select:{
-                            id:true,username:true,email:true,pic:true,isVerified:true
+                include: {
+                    users: {
+                        select: {
+                            id: true, username: true, email: true, pic: true, isVerified: true
                         }
                     }
                 }
@@ -152,35 +159,35 @@ class ChatRepository{
         }
     }
 
-    async addToGroup(data:AddOrRemoveInGroupChatDto){
+    async addToGroup(data: AddOrRemoveInGroupChatDto) {
         try {
             const chat = await prismaService.chat.findFirst({
-                where:{
-                    AND:[
-                        {isGroupChat:true},
-                        {id:data.chatId},
-                        {groupAdminId:data.loggedInUserId}
+                where: {
+                    AND: [
+                        { isGroupChat: true },
+                        { id: data.chatId },
+                        { groupAdminId: data.loggedInUserId }
                     ]
                 }
             });
 
-            if (!chat){
+            if (!chat) {
                 throw new Error("Group chat does not exist OR you are not the admin");
             }
 
             const updatedChat = await prismaService.chat.update({
-                where:{
-                    id:chat?.id
+                where: {
+                    id: chat?.id
                 },
-                data:{
-                    users:{
-                        connect:{id:data.userId}
+                data: {
+                    users: {
+                        connect: { id: data.userId }
                     }
                 },
-                include:{
-                    users:{
-                        select:{
-                            id:true,username:true,email:true,pic:true,isVerified:true
+                include: {
+                    users: {
+                        select: {
+                            id: true, username: true, email: true, pic: true, isVerified: true
                         }
                     }
                 }
@@ -192,35 +199,35 @@ class ChatRepository{
         }
     }
 
-    async removeFromGroup(data:AddOrRemoveInGroupChatDto){
+    async removeFromGroup(data: AddOrRemoveInGroupChatDto) {
         try {
             const chat = await prismaService.chat.findFirst({
-                where:{
-                    AND:[
-                        {isGroupChat:true},
-                        {id:data.chatId},
-                        {groupAdminId:data.loggedInUserId}
+                where: {
+                    AND: [
+                        { isGroupChat: true },
+                        { id: data.chatId },
+                        { groupAdminId: data.loggedInUserId }
                     ]
                 }
             });
 
-            if (!chat){
+            if (!chat) {
                 throw new Error("Group chat does not exist OR you are not the admin");
             }
 
             const updatedChat = await prismaService.chat.update({
-                where:{
-                    id:chat?.id
+                where: {
+                    id: chat?.id
                 },
-                data:{
-                    users:{
-                        disconnect:{id:data.userId}
+                data: {
+                    users: {
+                        disconnect: { id: data.userId }
                     }
                 },
-                include:{
-                    users:{
-                        select:{
-                            id:true,username:true,email:true,pic:true,isVerified:true
+                include: {
+                    users: {
+                        select: {
+                            id: true, username: true, email: true, pic: true, isVerified: true
                         }
                     }
                 }
